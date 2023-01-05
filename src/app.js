3b263452777478e538fb33bce4b7fb0d0e7629c1
JeanG1914 <jeanpaulgzea@gmail.com>
2023-01-05 23:31:54 +0000

    Generator Working

--- src/app.js
+++ src/app.js
@@ -1,4 +1,31 @@
 /* eslint-disable */
 import "./style.css";
 
-window.onload = function() {};
+window.onload = function() {
+  let cardid = document.querySelector(".value");
+  let suits = document.querySelectorAll(".suit");
+  let valuearray = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  let suitarray = ["♦", "♥", "♠", "♣"];
+  let randomsuit = suitarray[Math.floor(Math.random() * suitarray.length)];
+  suits.forEach(element => {
+    element.innerHTML = randomsuit;
+    if (randomsuit == "♦" || randomsuit == "♥") {
+      element.style.color = "red";
+    }
+  });
+  cardid.innerHTML = valuearray[Math.floor(Math.random() * valuearray.length)];
+};
